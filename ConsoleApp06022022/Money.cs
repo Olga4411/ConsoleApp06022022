84b48ddbb479a215944daa2885580e8ec5119c7f
Новичковская Ольга <miss4411@yandex.ru>
2022-02-06 14:48:57 +0700

    Add project files.

---
+++ ConsoleApp06022022/Money.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp06022022
+{
+   internal class Money
+    {
+       
+        //код валюты
+        public string CurrencyCode { get; set; }
+        //номинал валюты
+        public double Amount { get; set; }
+
+       public  Money(string currencyCode, double amount)
+        {
+            CurrencyCode = currencyCode;
+            Amount = amount;
+
+        }
+        public override string ToString()
+        {
+            return $"{Amount},{CurrencyCode}";
+        }
+
+        // Работа с делегатами
+        // описание делегата
+        // делегат, который хранит с 1 параметром double и результатом double
+        public delegate double MoneyConvertDelegate(double value);
+
+        public void MakeConvertOperation(MoneyConvertDelegate del)
+        {
+            if (del!=null)// если делегат не пустой был передан
+            {
+                double res = del(Amount); // выполняем конвертацию
+                Console.WriteLine($"{this}-converting->{res}");
+            }
+            else
+            {
+                Console.WriteLine("empty operation");
+            }
+        }
+    }
+}
---
+++ ConsoleApp06022022/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp06022022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Money money = new Money("test", 180);
+           // Money money = new Money("руб", 13890);
+            Console.WriteLine(money);
+
+            Console.ReadLine();
+            // 2 объект делегата
+            Money.MoneyConvertDelegate del = null;
+
+            //6/ добавить в делегат метод конвертации руб-> в доллар
+            del += delegate (double amount)
+              {
+                  return amount * 0.013;
+              };
+
+            // протестируем
+            money.MakeConvertOperation(del);
+            del += delegate (double amount)
+              {
+                  return amount * 0.37;
+              };
+
+            // лямбда
+            
+
+            money.MakeConvertOperation(del);
+
+
+        }
+    }
+}
